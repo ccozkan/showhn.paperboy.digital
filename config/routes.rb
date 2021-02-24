ccf253c47f32471e0955cc962ae5ff2ac1837113
Çağrı Özkan <cagrio.dev@gmail.com>
2021-02-24 15:28:59 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  resources :subscriptions, only: [:create]
+  get 'unsubscribe/:token', controller: 'unsubscriptions', action: 'show'
 end
