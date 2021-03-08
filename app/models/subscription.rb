ed0d5ffeef186358cbf72f4a5978d7b0e39a2741
Çağrı Özkan <cagrio.dev@gmail.com>
2021-03-09 01:05:18 +0300

    Validate format of email using regexp

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,4 +2,5 @@ class Subscription < ApplicationRecord
   has_secure_token :unsub_token
   validates_presence_of :email
   validates_uniqueness_of :email
+  validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -5,5 +5,7 @@ RSpec.describe Subscription, type: :model do
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email) }
     it { is_expected.to have_secure_token(:unsub_token) }
+    it { is_expected.to allow_value("email@addresse.foo").for(:email) }
+    it { is_expected.not_to allow_value("foo").for(:email) }
   end
 end
