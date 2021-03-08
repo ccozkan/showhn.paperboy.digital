bf87b2ba34d8970583c11d7e735240fad3d0eb35
Çağrı Özkan <cagrio.dev@gmail.com>
2021-03-09 00:55:18 +0300

    Add request_maker_service, its tests, remove post_receiver_services

---
+++ app/services/request_maker_service.rb
@@ -0,0 +1,15 @@
+class RequestMakerService
+  require 'httparty'
+
+  def initialize(url)
+    @url = url
+  end
+
+  def call
+    result = HTTParty.get(@url)
+  rescue HTTParty::Error => e
+    OpenStruct.new({ success?: false, error: e })
+  else
+    OpenStruct.new({ success?: true, payload: result })
+  end
+end
---
+++ spec/services/request_maker_service_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe RequestMakerService, type: :service do
+  describe '#call' do
+    let(:url) { 'https://ozkan.cc' }
+    let(:response) { RequestMakerService.new(url).call }
+
+    context 'when success' do
+
+      it do
+        expect(response.success?).to eq true
+        expect(response.payload).not_to eq nil
+        expect(response.error).to eq nil
+      end
+    end
+
+    context 'when not success' do
+      before do
+        allow(HTTParty).to receive(:get).and_raise(HTTParty::Error)
+      end
+
+      it do
+        expect(response.success?).to eq false
+        expect(response.payload).to eq nil
+        expect(response.error).not_to eq nil
+      end
+    end
+  end
+end
