5add351beb7eeadc8b378548c878537eef0d4a58
Çağrı Özkan <cagrio.dev@gmail.com>
2021-03-08 23:30:48 +0300

    Add footer partial and use them in mails

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   resources :subscriptions, only: [:create]
-  get 'unsubscribe/:token', controller: 'unsubscriptions', action: 'show'
+  get 'unsubscribe/:token', controller: 'unsubscriptions', action: 'show', as: 'unsubscription'
 end
