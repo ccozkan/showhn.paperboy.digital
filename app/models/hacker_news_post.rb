ae358d50f8bb571eb733b7274b6c258bf575464f
Çağrı Özkan <cagrio.dev@gmail.com>
2021-03-10 22:55:03 +0300

    Add class methods url determination and post filtering

--- app/models/hacker_news_post.rb
+++ app/models/hacker_news_post.rb
@@ -2,7 +2,15 @@ class HackerNewsPost < ApplicationRecord
   validates_presence_of :external_id
   validates_uniqueness_of :external_id
 
-  def self.posts_since_last_week
-    where('posted_at >= ?', 1.week.ago)
+  def self.score_ordered_posts_of_last_week
+    where('posted_at >= ?', 1.week.ago).order(score: :desc)
+  end
+
+  def self.api_url_for_item_details(item_id)
+    "https://hacker-news.firebaseio.com/v0/item/#{item_id}.json"
+  end
+
+  def self.api_url_for_show_stories
+    'https://hacker-news.firebaseio.com/v0/showstories.json'
   end
 end
