f295cc2d47f0aadbbe810839d3f158ad90d87095
Çağrı Özkan <cagrio.dev@gmail.com>
2021-03-18 22:21:06 +0300

    Notify honeybadger when service is not successful

--- app/workers/post_receiver_worker.rb
+++ app/workers/post_receiver_worker.rb
@@ -4,12 +4,18 @@ class PostReceiverWorker
   def perform(*args)
     api_url = HackerNewsPost.api_url_for_show_stories
     posts = RequestMakerService.new(api_url).call
-    return unless posts.success?
+    unless posts.success?
+      Honeybadger.notify(posts.errors)
+      return
+    end
 
     posts.payload.each do |item_id|
       api_url = HackerNewsPost.api_url_for_item_details(item_id)
       post_detail = RequestMakerService.new(api_url).call
-      next unless post_detail.success?
+      unless posts.success?
+        Honeybadger.notify(post_details.errors)
+        next
+      end
 
       post_detail = post_detail.payload.parsed_response
       next if HackerNewsPost.find_by(external_id: post_detail['id']).present?
