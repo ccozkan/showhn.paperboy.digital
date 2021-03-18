e40ded15d85b6267dc9b0321c911e819dd26d455
Çağrı Özkan <cagrio.dev@gmail.com>
2021-03-18 23:16:44 +0300

    Add http auth for sidekiq ui on production

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,14 @@ require 'sidekiq/web'
 require 'sidekiq/cron/web'
 
 Rails.application.routes.draw do
-  mount Sidekiq::Web => '/sidekiq'
+  scope :monitoring do
+    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+    end if Rails.env.production?
+
+    mount Sidekiq::Web, at: '/sidekiq'
+  end
 
   root 'pages#home'
   resources :subscriptions, only: [:create]
