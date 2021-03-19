aa079b356bd9280682a427b2b57e2d5806cb61e5
Çağrı Özkan <cagrio.dev@gmail.com>
2021-03-20 02:24:29 +0300

    Add confirmation methods

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,10 +1,16 @@
 class Subscription < ApplicationRecord
   has_secure_token :unsub_token
+  has_secure_token :confirm_token
+
   validates_presence_of :email
   validates_uniqueness_of :email
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
 
   after_commit :send_welcome_email, on: :create
+
+  def self.confirmed_subscriptions
+    where.not(confirmed_at: nil)
+  end
 
   def send_newsletter_email(posts)
     NewsletterMailer.newsletter(self, posts).deliver_later
@@ -12,5 +18,13 @@ class Subscription < ApplicationRecord
 
   def send_welcome_email
     NewsletterMailer.welcome(self).deliver_later
+  end
+
+  def confirm!
+    update!(confirmed_at: Time.current.utc)
+  end
+
+  def confirmed?
+    !!(confirmed_at)
   end
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -5,7 +5,36 @@ RSpec.describe Subscription, type: :model do
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_uniqueness_of(:email) }
     it { is_expected.to have_secure_token(:unsub_token) }
+    it { is_expected.to have_secure_token(:confirm_token) }
     it { is_expected.to allow_value("email@addresse.foo").for(:email) }
     it { is_expected.not_to allow_value("foo").for(:email) }
+  end
+
+  describe '#confirm?' do
+    context 'when true' do
+      let(:subscription) { create(:subscription, confirmed_at: nil)}
+
+      it { expect(subscription.confirmed?).to eq false }
+    end
+
+    context 'when false' do
+      let(:subscription) { create(:subscription, confirmed_at: Time.current.utc)}
+
+      it { expect(subscription.confirmed?).to eq true }
+    end
+  end
+
+  describe '#confirm!' do
+    let(:subscription) { create(:subscription, confirmed_at: nil)}
+    before { subscription.confirm! }
+
+    it { expect(subscription.confirmed?).to eq true }
+  end
+
+  describe '.confirmed_subscriptions' do
+    let(:not_confirmed_sub) { create(:subscription, confirmed_at: nil) }
+    let(:confirmed_sub) { create(:subscription) }
+
+    it { expect(Subscription.confirmed_subscriptions).to eq [confirmed_sub] }
   end
 end
