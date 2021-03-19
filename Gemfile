38f2d1a1e2383696f0fd250cf849c67eb8def95b
Çağrı Özkan <cagrio.dev@gmail.com>
2021-03-19 22:25:42 +0300

    Add factory_bot, factory_bot_rails

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 4.0.2'
   gem 'shoulda-matchers', '~> 4.0'
+  gem 'factory_bot'
+  gem 'factory_bot_rails'
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,6 +61,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include FactoryBot::Syntax::Methods
+
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
       with.test_framework :rspec
