f94f427aa5d78f9db136d9e4083783ce472414a0
Çağrı Özkan <cagrio.dev@gmail.com>
2021-03-19 22:29:59 +0300

    Add factories for subscription and hacker_news_post models

---
+++ spec/factories/hacker_news_posts.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :hacker_news_post do
+    url { "https://my-exciting-project.com" }
+    title { 'New Exciting Project' }
+    posted_at { Date.today - 5.days }
+    score { rand(1..500) }
+    external_id { SecureRandom.hex(5) }
+  end
+end
