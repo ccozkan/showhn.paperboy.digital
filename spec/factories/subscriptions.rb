6b3b4feb8e8fbfd53877b98e3d7f8efe5ea7b2da
Çağrı Özkan <cagrio.dev@gmail.com>
2021-03-19 22:33:24 +0300

    Remove skip_callback after build

--- spec/factories/subscriptions.rb
+++ spec/factories/subscriptions.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :subscription do
     email { "foo@bar.com" }
-    after(:build) {|subscription| subscription.class.skip_callback(:commit, :after, :send_welcome_email, raise: false)}
   end
 end
