107f6e5ed10df67e6b0f939eb79ad0b104859d83
Çağrı Özkan <cagrio.dev@gmail.com>
2021-03-19 23:02:44 +0300

    Add tests for NewsletterMailer methods

--- spec/mailers/newsletter_spec.rb
+++ spec/mailers/newsletter_spec.rb
@@ -1,5 +1,47 @@
 require "rails_helper"
 
+
 RSpec.describe NewsletterMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { ActiveJob::Base.queue_adapter = :test }
+  after { ActiveJob::Base.queue_adapter = :inline }
+
+  let(:subscription) { create(:subscription) }
+
+  describe '#welcome' do
+    context 'enqueued job is added' do
+      it do
+        expect { subscription.send_welcome_email }.to have_enqueued_job(ActionMailer::MailDeliveryJob).with('NewsletterMailer', 'welcome', 'deliver_now', { args: [subscription] } )
+      end
+    end
+
+    context 'email is sent' do
+      subject(:mailer) { described_class.welcome(subscription) }
+
+      it do
+        expect(mailer.from).to eq ['no-reply@showhn.paperboy.digital']
+        expect(mailer.subject).to include 'Welcome to'
+        expect(mailer.body.raw_source).to include ' enjoy '
+      end
+    end
+  end
+
+  describe '#newsletter' do
+    let(:post) { create(:hacker_news_post) }
+
+    context 'enqueued job is added' do
+      it do
+        expect { subscription.send_newsletter_email([post]) }.to have_enqueued_job(ActionMailer::MailDeliveryJob).with('NewsletterMailer', 'newsletter', 'deliver_now', { args: [subscription, [post]] })
+      end
+    end
+
+    context 'email is sent' do
+      subject(:mailer) { described_class.welcome(subscription) }
+
+      it do
+        expect(mailer.from).to eq ['no-reply@showhn.paperboy.digital']
+        expect(mailer.subject).to include 'Welcome to'
+        expect(mailer.body.raw_source).to include ' enjoy '
+      end
+    end
+  end
 end
