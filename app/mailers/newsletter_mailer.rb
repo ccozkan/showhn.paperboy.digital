baf3db78b14291abdde4a875f06a30f2467af4ff
Çağrı Özkan <cagrio.dev@gmail.com>
2021-03-19 21:26:15 +0300

    Use better naming in NewsletterMailer methods

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -1,10 +1,10 @@
 class NewsletterMailer < ApplicationMailer
-  def welcome_email(subscription)
+  def welcome(subscription)
     @subscription = subscription
     mail(to: @subscription.email, subject: 'Welcome to My Awesome Site')
   end
 
-  def newsletter_email(subscription, posts)
+  def newsletter(subscription, posts)
     @subscription = subscription
     @posts = posts
     mail(to: @subscription.email, subject: 'Last Week On Show HN!')
