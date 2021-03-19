42c93d35f597d42637a7d1c8d96e241d109759eb
Çağrı Özkan <cagrio.dev@gmail.com>
2021-03-20 02:05:05 +0300

    Add confirm/:token path

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,10 @@ Rails.application.routes.draw do
   end
 
   root 'pages#home'
+
   resources :subscriptions, only: [:create]
   get 'unsubscribe/:token', controller: 'unsubscriptions', action: 'show', as: 'unsubscription'
+  get 'confirm/:token', controller: 'confirm_subscriptions', action: 'show', as: 'confirm_subscription'
+
   get '/check.txt', to: proc { [200, {}, ['it_works']] }
 end
