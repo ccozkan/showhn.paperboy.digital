648fdcf7dcf31befe7b0b77bf9470d87d7c048d8
Çağrı Özkan <cagrio.dev@gmail.com>
2021-03-20 02:25:57 +0300

    Send newsletters just to confimed subs

--- app/workers/newsletter_sender_worker.rb
+++ app/workers/newsletter_sender_worker.rb
@@ -3,8 +3,10 @@ class NewsletterSenderWorker
 
   def perform(*args)
     posts = HackerNewsPost.score_ordered_posts_of_last_week
-    Subscription.all.each do |subscription|
-      subscription.send_newsletter_email(posts)
+    subscriptions = Subscription.confirmed_subscriptions
+
+    subscriptions.each do |sub|
+      sub.send_newsletter_email(posts)
     end
   end
 end
