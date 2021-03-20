fe52edabece33bfd068e4873706b1299c8909cca
Çağrı Özkan <cagrio.dev@gmail.com>
2021-03-20 09:51:16 +0300

    Cleanup request tests

--- spec/requests/subsriptions_request_spec.rb
+++ spec/requests/subsriptions_request_spec.rb
@@ -8,9 +8,5 @@ RSpec.describe 'Subscriptions', type: :request do
       expect(response).to have_http_status(:redirect)
       expect(response).to redirect_to(root_path)
     end
-
-    it 'creates its subscription record' do
-      expect(Subscription.find_by(email: 'foo@bar.com')).not_to eq nil
-    end
   end
 end
