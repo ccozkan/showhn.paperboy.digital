5d936ac9a77c2e8c7b53ed13678229dcc3ebe6b9
Çağrı Özkan <cagrio.dev@gmail.com>
2021-03-20 09:50:54 +0300

    Update mail subject

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -1,7 +1,7 @@
 class NewsletterMailer < ApplicationMailer
   def welcome(subscription)
     @subscription = subscription
-    mail(to: @subscription.email, subject: 'Welcome to My Awesome Site')
+    mail(to: @subscription.email, subject: 'Please confirm your subscription')
   end
 
   def newsletter(subscription, posts)
