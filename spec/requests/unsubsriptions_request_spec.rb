dbeea2222cc649c834beca09067b86adb76954ce
Çağrı Özkan <cagrio.dev@gmail.com>
2021-03-20 09:51:16 +0300

    Cleanup request tests

--- spec/requests/unsubsriptions_request_spec.rb
+++ spec/requests/unsubsriptions_request_spec.rb
@@ -9,9 +9,5 @@ RSpec.describe 'Unsubscriptions', type: :request do
       expect(response).to have_http_status(:redirect)
       expect(response).to redirect_to(root_path)
     end
-
-    it 'deletes its subscription' do
-      expect(Subscription.find_by(email: subscription.email)).to eq nil
-    end
   end
 end
