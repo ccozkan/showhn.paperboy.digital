bd6bab3c60553e2d3077fc55948c26f154360e0c
Çağrı Özkan <cagrio.dev@gmail.com>
2021-03-21 16:03:02 +0300

    Change development env default mailer url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost',
-                                               port: 3000 }
+                                               port: 5100 }
 
   config.action_mailer.smtp_settings = { address: 'localhost',
                                          port: 1025 }
