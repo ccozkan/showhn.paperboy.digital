55da12bedbee85227acdc8cd886d153e887c1764
Çağrı Özkan <cagrio.dev@gmail.com>
2021-03-24 17:42:16 +0300

    Cover request test for failure of .save

--- spec/requests/subsriptions_request_spec.rb
+++ spec/requests/subsriptions_request_spec.rb
@@ -2,11 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Subscriptions', type: :request do
   describe 'POST' do
-    before { post '/subscriptions', params: { email: 'foo@bar.com' }}
+    context 'when success' do
+      before { post '/subscriptions', params: { email: 'foo@bar.com' } }
 
-    it 'gets redirected' do
-      expect(response).to have_http_status(:redirect)
-      expect(response).to redirect_to(root_path)
+      it 'gets redirected' do
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'when failure' do
+      before { post '/subscriptions', params: { email: 'invalid_email_address' } }
+
+      it do
+        expect(response).to have_http_status(:redirect)
+        expect(response).to redirect_to(root_path)
+        expect(flash[:alert]).to include 'Email is invalid'
+      end
     end
   end
 end
