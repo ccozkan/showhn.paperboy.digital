d854636eda333045f150d38b0c1b883b6fce7555
Çağrı Özkan <cagrio.dev@gmail.com>
2021-04-03 23:29:06 +0300

    Change focused time range of .score_prdered_posts_of_last_week

--- app/models/hacker_news_post.rb
+++ app/models/hacker_news_post.rb
@@ -2,10 +2,6 @@ class HackerNewsPost < ApplicationRecord
   validates_presence_of :external_id
   validates_uniqueness_of :external_id
 
-  def self.score_ordered_posts_of_last_week
-    where('posted_at > ?', 1.week.ago.beginning_of_day + 12.hours).order(score: :desc).to_a
-  end
-
   def self.api_url_for_item_details(item_id)
     "https://hacker-news.firebaseio.com/v0/item/#{item_id}.json"
   end
@@ -22,5 +18,24 @@ class HackerNewsPost < ApplicationRecord
     posted_at = DateTime.strptime(post_detail['time'].to_s, '%s')
 
     { url: url, score: score, title: title, posted_at: posted_at, external_id: external_id }
+  end
+
+  def self.score_ordered_posts_of_last_week
+    starting_at = self.last_week_time_period[:starting_at]
+    ending_at = self.last_week_time_period[:ending_at]
+
+    where('posted_at > ?', starting_at).where('posted_at <= ?', ending_at).order(score: :desc)
+  end
+
+  def self.top20_posts_of_last_week
+    self.score_ordered_posts_of_last_issue.limit(20).to_a
+  end
+
+  def self.last_week_time_period
+    starting_at = Date.today.prev_occurring(:wednesday).beginning_of_day + 12.hours
+    {
+      starting_at: starting_at,
+      ending_at: starting_at + 7.days
+    }
   end
 end
