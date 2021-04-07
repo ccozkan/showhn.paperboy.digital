2289f3126a3cf3682c7179194229c3b847f38bbe
Çağrı Özkan <cagrio.dev@gmail.com>
2021-04-07 15:37:28 +0300

    Cover model tests for post filterer & orderer methods

--- spec/models/hacker_news_post_spec.rb
+++ spec/models/hacker_news_post_spec.rb
@@ -6,13 +6,25 @@ RSpec.describe HackerNewsPost, type: :model do
     it { is_expected.to validate_uniqueness_of(:external_id) }
   end
 
-  describe '.score_ordered_posts_of_last_week' do
+  describe do
     let!(:most_popular_post) { create(:hacker_news_post, score: 100, posted_at: Date.today - 2.days) }
     let!(:less_popular_post) { create(:hacker_news_post, score: 10, posted_at: Date.today - 1.days) }
     let!(:last_weeks_popular_post) { create(:hacker_news_post, score: 10, posted_at: (Date.today - 7.days).beginning_of_day + 12.hours) }
+    let!(:other_posts) { create_list(:hacker_news_post, 42, score: 5, posted_at: Date.today - 3.days) }
 
-    let!(:subject) { HackerNewsPost.score_ordered_posts_of_last_week}
+    describe '.score_ordered_posts_of_last_week' do
+      let!(:subject) { HackerNewsPost.score_ordered_posts_of_last_week}
 
-    it { expect(subject).to eq [most_popular_post, less_popular_post] }
+      it { expect(subject[0..1]).to eq [most_popular_post, less_popular_post] }
+      it { expect(subject).not_to include last_weeks_popular_post }
+      it { expect(subject.size).not_to eq 42 + 3 }
+    end
+
+    describe  '.top20_posts_of_last_week' do
+      let!(:subject) { HackerNewsPost.top20_posts_of_last_week }
+
+      it { expect(subject.class).to eq Array }
+      it { expect(subject.size).to eq  20 }
+    end
   end
 end
