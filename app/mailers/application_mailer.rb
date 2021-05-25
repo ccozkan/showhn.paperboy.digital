1d599809dd775c1e0d447b132f4515156c5d18e8
Çağrı Özkan <cagrio.dev@gmail.com>
2021-05-25 03:30:16 +0300

    Set fancier email defaults

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'no-reply@showhn.paperboy.digital'
+  default from: "PaperboyDigital <no-reply@showhn.paperboy.digital>"
+  default reply_to: "PaperboyDigital <hey@paperboy.digital>"
   layout 'mailer'
 end
 
