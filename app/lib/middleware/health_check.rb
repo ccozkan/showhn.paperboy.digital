7b1cc72ef3cb5fdd5f3639d6ce0d7678d82b467b
Çağrı Özkan <ccozkan@tutanota.com>
2022-07-05 23:54:58 +0300

    Remove duration

--- app/lib/middleware/health_check.rb
+++ app/lib/middleware/health_check.rb
@@ -7,15 +7,7 @@ module Middleware
     end
 
     def call(env)
-      request_started_on = Time.current
       status, headers, response = @app.call(env)
-      request_ended_on = Time.current
-      duration = request_ended_on - request_started_on
-
-      modified_response = JSON.parse(response.first)
-      modified_response['duration'] = duration.to_s
-      response = [modified_response.to_json]
-
       [status, headers, response]
     end
   end
